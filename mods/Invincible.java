b01aba9cbe345f29f7843a4e69c42fc58ffbcc44
nicholas <nicholas@f606c557-477b-0338-b6ab-cd7775427d7d>
2011-01-08 03:12:16 +0000

    Added invincible mod

---
+++ mods/Invincible.java
@@ -0,0 +1,52 @@
+import com.mcmod.Loader;
+import com.mcmod.api.Mod;
+import com.mcmod.inter.Player;
+
+
+public class Invincible implements Mod {
+	@Override
+	public boolean isTogglable() {
+		return true;
+	}
+
+	@Override
+	public String getName() {
+		return "Invincible";
+	}
+
+	@Override
+	public String getDescription() {
+		return "You'll no longer be hurt by anything.";
+	}
+
+	@Override
+	public void enable() {
+	}
+
+	@Override
+	public void disable() {
+	}
+
+	@Override
+	public void render() {
+		Player p = Loader.getMinecraft().getPlayer();
+		
+		if(p != null) {
+			if(p.getAirTimer() < 300) {
+				p.setAirTimer(300);
+			}
+			
+			if(p.getFireTimer() != -20) {
+				p.setFireTimer(-20);
+			}
+			
+			if(p.getHealth() < 20) {
+				p.setHealth(20);
+			}
+			
+			if(p.getFallDistance() != 0.0f) {
+				p.setFallDistance(0.0f);
+			}
+		}
+	}
+}
