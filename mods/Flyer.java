928291c397d0d44fc7e937609448979564911ae8
jmluismoreno <jmluismoreno@f606c557-477b-0338-b6ab-cd7775427d7d>
2011-01-20 04:33:06 +0000

    Loads mods from user.home\McMod\mods alongside the Mod from JAR loading For example, on Windows 7 it would be: "C:\User\****\McMod\mods"
    Modification to Flyer, now acts as a "noclip" and when the left shift is pressed, you will descend slowly
    Modified by cjz

--- mods/Flyer.java
+++ mods/Flyer.java
@@ -1,38 +1,45 @@
+
 import org.lwjgl.input.Keyboard;
 
 import com.mcmod.Loader;
 import com.mcmod.api.Mod;
 import com.mcmod.inter.Player;
 
-
 public class Flyer implements Mod {
-	@Override
-	public boolean isTogglable() {
-		return true;
-	}
 
-	@Override
-	public String getName() {
-		return "Flyer";
-	}
+    @Override
+    public boolean isTogglable() {
+        return true;
+    }
 
-	@Override
-	public String getDescription() {
-		return "Allows you to hold \"SPACE\" and fly.";
-	}
+    @Override
+    public String getName() {
+        return "Flyer";
+    }
 
-	@Override
-	public void process() {
-		Keyboard.enableRepeatEvents(true);
-		Player p = Loader.getMinecraft().getPlayer();
-		
-		if(p != null) {
-			if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-				p.setSpeedY(0.5d);
-			} 
-			
-			p.setFallDistance(0.0f);
-		}
-	}
+    @Override
+    public String getDescription() {
+        return "Allows you to hold \"SPACE\" and fly.";
+    }
 
+    @Override
+    public void process() {
+        Keyboard.enableRepeatEvents(true);
+        Player p = Loader.getMinecraft().getPlayer();
+        
+        if (p != null) {
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+                p.setSpeedY(0.5d);
+                p.setFallDistance(0.0f);
+            } else {
+                if (p.getOnGround() == false) {
+                    if (!Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+                        p.setSpeedY(0d);
+                    }
+                    p.setFallDistance(0.0f);
+                }
+            }
+
+        }
+    }
 }
